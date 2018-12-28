5d6f8a4af3481cce83ce2afb6f669e762951c176
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2018-12-28 21:27:50 +0530

    Java Handling Program

---
+++ 02-BasicStringHandling/StringHandling.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class StringHandling {
+
+    public static void main(String[] args) {
+        String firstName = "Joan";
+        String lastName = "Smith";
+
+        System.out.println("Concatenation:" + firstName + " - " +  lastName);
+
+        System.out.println("New line: \n" + firstName + " " + lastName);
+
+        System.out.println("Tab: \t" + firstName + " " + lastName);
+
+        System.out.println("Backward: \b" + firstName + " " + lastName);
+
+        System.out.println("Carriage return: \r" + firstName + " " + lastName);
+
+        System.out.println("Simple quote: \'" + firstName + " " + lastName + "\'");
+
+        System.out.println("Double quote: \"" + firstName + " " + lastName + "\"");
+
+    }
+}
