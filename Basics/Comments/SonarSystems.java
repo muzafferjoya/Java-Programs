205c66d16e1e0d2747121d2f5c1bea6f4b9ad664
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2018-12-29 18:44:55 +0530

    Java Basics Program

---
+++ Basics/Comments/SonarSystems.java
@@ -0,0 +1,24 @@
+// Awesome application
+
+
+public class SonarSystems {
+
+	// This is the main method
+	public static void main(String[] args) {
+		
+		//System.out.println("Print Line");
+		// This is a print without a new line
+		System.out.print("Print\n");
+		
+		
+		System.out.println("Print Line 3");
+		System.out.println("Print Line 3");
+		System.out.println("Print Line 3");
+		System.out.println("Print Line 3");
+		System.out.println("Print Line 3");
+		System.out.println("Print Line 3");
+		
+		
+	}
+
+}
